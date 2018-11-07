05d488fe36937ff7ba8244e500b3d31fb41773c1
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-07 10:20:36 +0300

    fix syntax error line 22

--- tests/test_views.py
+++ tests/test_views.py
@@ -18,7 +18,7 @@ def test_login_(client):
         auth = Auth(username, user_pass, role)
         res = client.post('/api/v1/auth/login', data = json.dumps(dict(
             use_rname = auth.user_name,
-            password = auth.password
+            password = auth.password,
             role = auth.role
         )),content_type='application/json')
         assert res.status_code == 200
