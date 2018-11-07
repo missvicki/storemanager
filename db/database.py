40ca26197c7fd9c1c05371f144eccacb72eb28b7
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-07 17:41:42 +0300

    remove testing config

--- db/database.py
+++ db/database.py
@@ -12,21 +12,13 @@ class DatabaseConnection:
     """Connect to the database"""
     def __init__(self):
         try:
-            if app.config["TESTING"]:
-                self.conn = psycopg2.connect(host="localhost", 
-                                             database="storemanager_test_db", 
-                                             user="postgres", 
-                                             password="admin",
-                                             port="5432")
-            else:
-                self.conn = psycopg2.connect(host="ec2-54-83-27-162.compute-1.amazonaws.com", 
-                                             database="dcojjie21dvmis", 
-                                             user="sejawvmmzwabhv", 
-                                             port = "5432",
-                                             password="26c76331cf6695b3226de7db2d6405f757329228c9e84b858a29847e030d6044")
+            self.conn = psycopg2.connect(host="ec2-54-83-27-162.compute-1.amazonaws.com", 
+                                            database="dcojjie21dvmis", 
+                                            user="sejawvmmzwabhv", 
+                                            port = "5432",
+                                            password="26c76331cf6695b3226de7db2d6405f757329228c9e84b858a29847e030d6044")
             self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
             self.conn.autocommit = True
-    
         except psycopg2.DatabaseError as anything:
             print (anything)
         
