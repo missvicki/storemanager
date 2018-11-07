e749e448b1a4e466776990157d47bc9c94054b42
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-07 10:18:15 +0300

    test login

---
+++ tests/test_views.py
@@ -0,0 +1,24 @@
+from flask import json
+from flask_jwt_extended import create_access_token
+import pytest
+from api.views import app
+from models.usersModel import Login
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+def test_login_(client):
+    with app.app_context():
+        username = 'vickib'
+        user_pass = 'vibel'
+        role = 'admin'
+        auth = Auth(username, user_pass, role)
+        res = client.post('/api/v1/auth/login', data = json.dumps(dict(
+            use_rname = auth.user_name,
+            password = auth.password
+            role = auth.role
+        )),content_type='application/json')
+        assert res.status_code == 200
