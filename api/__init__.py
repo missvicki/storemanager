25057c0d73da1605822bfd9791e35e4a04d4c9aa
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-10 17:02:51 +0300

    add index route and render index html

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
 
-def create_app(config_name):
-    app = Flask(__name__)
-    # Setup flask-jwt-extended
-    app.config['JWT_SECRET_KEY'] = 'secret'
-    app.config.from_object(config_name)
-    jwt = JWTManager(app)
-    return app
-
-app = create_app('config.TestingConfig')
-
+app = Flask(__name__)
 
 from api import views
+
+# Setup flask-jwt-extended
+app.config['JWT_SECRET_KEY'] = 'secret'
+jwt = JWTManager(app)
+
+# index route
+@app.route('/')
+def hello():
+    """my home"""
+    return render_template("index.html")
+    
