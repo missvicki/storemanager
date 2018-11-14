7f116e183e655827949fa6033a98d5c3bc57918b
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-14 10:19:31 +0300

    implement cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ from api import views
 # Setup flask-jwt-extended
 app.config['JWT_SECRET_KEY'] = 'secret'
 jwt = JWTManager(app)
+CORS(app)
 
 # index route
 @app.route('/')
