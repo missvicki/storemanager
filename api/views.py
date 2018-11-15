1d3792517c457ff5937d641e2806f09450765d16
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-15 14:05:33 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -12,6 +12,7 @@ from api.validations.validations import (validate_product, validate_product_modi
 
 # what happens when you start the app
 database = DatabaseConnection()
+database.default_admin()
 
 #error handlers
 @app.errorhandler(404)
