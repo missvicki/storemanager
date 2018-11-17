f6beec91268c4375da49cd9ef835f76506ecf790
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-17 15:15:56 +0300

    config error remove

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from config import app_config
 app = Flask(__name__, instance_relative_config=True)
 
 from api import views
@@ -13,9 +12,6 @@ app.config['JWT_SECRET_KEY'] = 'secret'
 # against the blacklist
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
-
-#enable environment
-app.config.from_object(app_config["development"])
 
 jwt = JWTManager(app)
 
